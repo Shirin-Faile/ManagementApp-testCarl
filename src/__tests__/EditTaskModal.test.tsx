06d1ebb7be447e29ba3f8311515523f82e5f28d8
shirin faile <shirin.faile@hotmail.com>
2024-09-19 08:48:01 +0200

    fixed import

--- src/__tests__/EditTaskModal.test.tsx
+++ src/__tests__/EditTaskModal.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen, fireEvent } from "@testing-library/react";
-import EditTaskModal from "../components/EditTaskModal";
+import EditTaskModal from "@/components/EditTaskModal";
 
 const mockTask = {
   id: 1,
